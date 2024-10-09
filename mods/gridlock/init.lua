717eef0deb9eac8310ba6eb16bf4a6ec6e8c04fe
jgordon510 <jgordon.oakland@gmail.com>
2024-10-08 21:41:49 -0700

    move music to file

--- mods/gridlock/init.lua
+++ mods/gridlock/init.lua
@@ -5,6 +5,7 @@ local COLOR_INPUT_POS =  {x=1, y=-1, z=0}
 
 Gridlock = {}
 dofile(modpath .. "/global.lua")
+dofile(modpath .. "/music.lua")
 
 local function isSamePos(pos1, pos2)
     return pos1.x == pos2.x and pos1.y == pos2.y and pos1.z == pos2.z
@@ -868,53 +869,6 @@ minetest.register_globalstep(function(dtime)
         end
     end
 end)
-
-
--- Table mapping each board number to the corresponding sound file
-local room_sounds = {
-    [1] = {key = "cloud_8", gain = 0.5},
-    [2] = {key = "planet_metal", gain = 0.5},
-    [3] = {key = "el_grande_diamonte_del_mundo_profundo", gain = 0.6},
-    [4] = {key = "three_chord_rock", gain = 0.5},
-    [5] = {key = "lord_of_happy", gain = 0.2},
-}
-
--- Variable to store the current sound handle
-local current_handle = nil
-
--- Function to update room audio
-local function update_room_audio(board_n)
-    -- Stop the currently playing sound with a fade-out effect (if possible)
-    if current_handle then
-        minetest.sound_fade(current_handle, 0.5, 0.0) -- Adjust fade-out duration if needed
-    end
-
-    -- Get the sound for the new room
-    local sound = room_sounds[board_n]
-    if sound then
-        -- Play the new sound on loop
-
-        current_handle = minetest.sound_play( {
-            name = sound.key,
-            gain = sound.gain,
-            loop = true,
-        })
-    end
-end
-
--- Call this function whenever Gridlocks.board_n changes
--- For example:
-minetest.after(3, function() 
-    minetest.register_globalstep(function()
-        local board_n = Gridlock.board_n
-        if board_n ~= Gridlock.last_board_n then
-            Gridlock.last_board_n = board_n
-            update_room_audio(board_n)
-        end
-        
-    end)
-end)
-
 
 --debug command for setting progress
 minetest.register_chatcommand("gl-progress", {
---
+++ mods/gridlock/music.lua
@@ -0,0 +1,43 @@
+-- Table mapping each board number to the corresponding sound file
+local room_music = {
+    [1] = {key = "cloud_8", gain = 0.5},
+    [2] = {key = "planet_metal", gain = 0.5},
+    [3] = {key = "el_grande_diamonte_del_mundo_profundo", gain = 0.6},
+    [4] = {key = "three_chord_rock", gain = 0.5},
+    [5] = {key = "lord_of_happy", gain = 0.2},
+}
+-- Variable to store the current sound handle
+local current_handle = nil
+
+-- Function to update room audio
+local function update_room_audio(board_n)
+    -- Stop the currently playing sound with a fade-out effect (if possible)
+    if current_handle then
+        minetest.sound_fade(current_handle, 0.5, 0.0) -- Adjust fade-out duration if needed
+    end
+
+    -- Get the sound for the new room
+    local sound = room_music[board_n]
+    if sound then
+        -- Play the new sound on loop
+
+        current_handle = minetest.sound_play( {
+            name = sound.key,
+            gain = sound.gain,
+            loop = true,
+        })
+    end
+end
+
+-- Call this function whenever Gridlocks.board_n changes
+-- For example:
+minetest.after(3, function() 
+    minetest.register_globalstep(function()
+        local board_n = Gridlock.board_n
+        if board_n ~= Gridlock.last_board_n then
+            Gridlock.last_board_n = board_n
+            update_room_audio(board_n)
+        end
+        
+    end)
+end)
